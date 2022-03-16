51dc468a1bd95f6312b825814cd16ead0b631816
Spencer Ferris <git@spencewenski.com>
2022-03-16 00:46:20 -0700

    Run 'cargo fmt'

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789";
     let mut rng = rand::thread_rng();
 
-    let suffix   = if args.random_suffix {
-        let s : String = (0..args.random_suffix_length)
+    let suffix = if args.random_suffix {
+        let s: String = (0..args.random_suffix_length)
             .map(|_| {
                 let i = rng.gen_range(0, CHARSET.len());
                 CHARSET[i] as char
@@ -27,22 +27,21 @@ fn main() {
 
 #[derive(Debug)]
 pub struct Arguments {
-    pub domain : String,
-    pub email_domain : String,
-    pub random_suffix : bool,
-    pub random_suffix_length : u32,
-    pub separator : String,
+    pub domain: String,
+    pub email_domain: String,
+    pub random_suffix: bool,
+    pub random_suffix_length: u32,
+    pub separator: String,
 }
 
 impl Arguments {
-
     fn new() -> Arguments {
         Arguments {
-            domain : String::new(),
-            email_domain : String::new(),
-            random_suffix : true,
-            random_suffix_length : 10,
-            separator : "_".to_string(),
+            domain: String::new(),
+            email_domain: String::new(),
+            random_suffix: true,
+            random_suffix_length: 10,
+            separator: "_".to_string(),
         }
     }
 
@@ -54,31 +53,38 @@ impl Arguments {
             ap.set_description("Generate a new email alias for the given domain.");
 
             ap.refer(&mut args.domain)
-                .add_option(&["-d", "--domain"],
-                            Store,
-                            "Name of the domain to create an email alias for, e.g. mozilla")
+                .add_option(
+                    &["-d", "--domain"],
+                    Store,
+                    "Name of the domain to create an email alias for, e.g. mozilla",
+                )
                 .required();
 
             ap.refer(&mut args.email_domain)
-                .add_option(&["-e", "--email"],
-                            Store,
-                            "Name of the base email domain, e.g. spencewenski.com")
+                .add_option(
+                    &["-e", "--email"],
+                    Store,
+                    "Name of the base email domain, e.g. spencewenski.com",
+                )
                 .required();
 
-            ap.refer(&mut args.random_suffix)
-                .add_option(&["-n", "--no-suffix"],
-                            StoreFalse,
-                            "Disable the random suffix.");
+            ap.refer(&mut args.random_suffix).add_option(
+                &["-n", "--no-suffix"],
+                StoreFalse,
+                "Disable the random suffix.",
+            );
 
-            ap.refer(&mut args.random_suffix_length)
-                .add_option(&["-l", "--random-suffix-length"],
-                            Store,
-                            "Length of the random suffix. Default is 10.");
+            ap.refer(&mut args.random_suffix_length).add_option(
+                &["-l", "--random-suffix-length"],
+                Store,
+                "Length of the random suffix. Default is 10.",
+            );
 
-            ap.refer(&mut args.separator)
-                .add_option(&["-s", "--separator"],
-                            Store,
-                            "Separator between the domain and the random suffix. Default is '_'");
+            ap.refer(&mut args.separator).add_option(
+                &["-s", "--separator"],
+                Store,
+                "Separator between the domain and the random suffix. Default is '_'",
+            );
 
             ap.parse_args_or_exit();
         }
@@ -86,5 +92,3 @@ impl Arguments {
         args
     }
 }
-
-
